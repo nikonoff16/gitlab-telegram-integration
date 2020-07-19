fbec1409b37dca6990cb57cb79f2031df5c7793b
Joseph Nikonoff <i.nikonoff16@gmail.com>
2020-07-19 16:41:12 +0500

    репозиторий инициализирован

---
+++ src/main/java/ru/hostco/dvs/gs/gitlabtelegramintegration/GitlabTelegramIntegrationApplication.java
@@ -0,0 +1,13 @@
+package ru.hostco.dvs.gs.gitlabtelegramintegration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitlabTelegramIntegrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitlabTelegramIntegrationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ru/hostco/dvs/gs/gitlabtelegramintegration/GitlabTelegramIntegrationApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hostco.dvs.gs.gitlabtelegramintegration;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitlabTelegramIntegrationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
