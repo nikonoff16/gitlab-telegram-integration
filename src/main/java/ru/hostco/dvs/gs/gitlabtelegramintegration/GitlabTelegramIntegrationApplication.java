359063a75c38c73e9250e6bad7b5f7bf7d804cb0
Joseph Nikonoff <i.nikonoff16@gmail.com>
2020-07-23 23:11:02 +0500

    App can access my Gitlab and take merge requests our there

--- src/main/java/ru/hostco/dvs/gs/gitlabtelegramintegration/GitlabTelegramIntegrationApplication.java
+++ src/main/java/ru/hostco/dvs/gs/gitlabtelegramintegration/GitlabTelegramIntegrationApplication.java
@@ -2,7 +2,11 @@ package ru.hostco.dvs.gs.gitlabtelegramintegration;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
+@EnableScheduling
+@EnableFeignClients
 @SpringBootApplication
 public class GitlabTelegramIntegrationApplication {
 
--- src/main/java/ru/hostco/dvs/gs/gitlabtelegramintegration/service/Syncer.java
+++ src/main/java/ru/hostco/dvs/gs/gitlabtelegramintegration/service/Syncer.java
@@ -1,12 +1,31 @@
 package ru.hostco.dvs.gs.gitlabtelegramintegration.service;
 
+import lombok.AllArgsConstructor;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
+import ru.hostco.dvs.gs.gitlabtelegramintegration.config.ApplicationConfiguration;
+import ru.hostco.dvs.gs.gitlabtelegramintegration.dto.MergeRequestInfo;
+
+import java.util.Collection;
 
 @Slf4j
 @Service
+@RequiredArgsConstructor(onConstructor_ = {@Autowired})
 public class Syncer {
-    public Syncer() {
-        log.info("Hello world");
+
+    private final ApplicationConfiguration applicationConfiguration;
+    private final GitLabClient gitLabClient;
+
+    @Scheduled(fixedDelay = 5 * 1000)
+    public void updateMergeRequests() {
+        final Collection<MergeRequestInfo> mergeRequests = gitLabClient.
+                getMergeRequests(applicationConfiguration.getGitLabPrivateToken());
+
+
+        log.info("mergeRequests");
     }
+
 }
